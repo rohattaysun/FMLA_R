ce7a00496fc8e9ce53aae794c42bb1a7c411b1b8
rohat taysun <rtaysu01@student.bbk.ac.uk>
2025-11-05 17:59:22 +0000

    Week 6 practices

--- Weekly_Materials/Week5.R
+++ Weekly_Materials/Week5.R
@@ -1,3 +1,18 @@
 #Week 5 Material Code Practices
-#Exponential smoothing
+ #Exponential smoothing
+  #Simple Exponential Smoothing
+   #Optimizing smoothing parameters
 
+algeria_economy
+algeria_economy <- global_economy %>%
+  filter(Country == "Algeria")
+
+algeria_economy %>%
+  autoplot(Population)
+
+View(algeria_economy %>%
+       autoplot(Population))
+
+algeria_economy %>%
+  autoplot(Exports) +
+  labs(y = "% of GDP", title = "Exports: Algeria")
